bd88112c6f85c0702d13168b91ffa5fa44232053
Tiago Weintraub <t.weintraub@alumno.um.edu.ar>
2024-08-15 20:16:55 -0300

    [Update] Conexiones IPV4 e IPV6 funcionando.

--- app/cliente.py
+++ app/cliente.py
@@ -1,13 +1,30 @@
 import socket
 
 class Cliente:
-    def __init__(self, host='localhost', port=5005):
-        self.host = host
+    def __init__(self, host_ipv4='127.0.0.1', host_ipv6='::1', port=5005):
+        self.host_ipv4 = host_ipv4
+        self.host_ipv6 = host_ipv6
         self.port = port
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = None
         self.conexion = False
 
+    def configurar_conexion(self):
+        tipo_conexion = input("¿Desea usar IPv4 o IPv6? (4/6): ").strip()
+        if tipo_conexion == '4':
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.host = self.host_ipv4
+        elif tipo_conexion == '6':
+            self.client_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+            self.host = self.host_ipv6
+        else:
+            print("Opción no válida. Por favor, elija '4' para IPv4 o '6' para IPv6.")
+            return False
+        return True
+
     def conectar(self):
+        if not self.configurar_conexion():
+            return
+
         try:
             self.conexion = True
             self.client_socket.connect((self.host, self.port))
@@ -48,4 +65,3 @@ if __name__ == "__main__":
             break
 
         cliente.enviar_isbn(isbn) # Enviar el ISBN al servidor
-
