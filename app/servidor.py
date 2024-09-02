fc436f99086cf55cc5e191a15cc893792c7fa788
Tiago Weintraub <t.weintraub@alumno.um.edu.ar>
2024-09-02 14:45:58 -0300

    [Refactor] Sockets corriendo en procesos hijos.

--- app/servidor.py
+++ app/servidor.py
@@ -69,10 +69,9 @@ class Servidor:
     def __init__(self, host=None, port=5555, log_queue=None):
         self.host = host
         self.port = port
-        self.contador_clientes = 0
         self.log_queue = log_queue
-        self.server_sockets = []  # Lista para almacenar los sockets creados (IPv4 e IPv6).
-
+        self.puerto_cliente = 0
+        self.server_sockets = []
 
     def enviar_logs(self, tipo, mensaje):
         log_entry = {
@@ -83,75 +82,80 @@ class Servidor:
 
     def start_server(self):
         try:
+            print("PID del servidor:", os.getpid())
             addrinfos = socket.getaddrinfo(self.host, self.port, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE)
-            n = 0
+
             for addrinfo in addrinfos:
-                print(f"\nAddrinfo {n}: {addrinfo}")
-                n += 1
-                family, socktype, proto, canonname, sockaddr = addrinfo
-                try:
-                    server_socket = socket.socket(family, socktype, proto)   
-                    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Permite reutilizar la dirección del socket.
+                af, socktype, proto, canonname, sockaddr = addrinfo
 
-                    if family == socket.AF_INET6: # Si la familia es IPv6, se configura un socket que solo acepte conexiones IPv6.
-                        server_socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
-                        address_type = "IPv6"
-                    else: # Si la familia es IPv4, se configura un socket que solo acepte conexiones IPv4.
-                        address_type = "IPv4"
+                pid = os.fork()
+                if pid == 0:
+                    print(f'\nProceso Hijo {os.getpid()}')
 
-                    server_socket.bind(sockaddr)
-                    server_socket.listen(5)
+                    try:
+                        server_socket = socket.socket(af, socktype, proto)
+                        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+                        server_socket.bind(sockaddr)
+                        server_socket.listen(5)
+
+                        if af == socket.AF_INET6:
+                            address_type = "IPv6"
+                        else:
+                            address_type = "IPv4"
+
+                        print(f'Servidor  escuchando en {sockaddr} ({address_type})')
+                        self.enviar_logs("INFO", f"Servidor iniciado en {sockaddr[0]}:{self.port} ({address_type})")
+
+                        while True:
+                            conn, res = server_socket.accept()
+
+                            
+                            if af == socket.AF_INET6:
+                                print(f'\nCliente conectado usando IPv6 desde {res}')
+                                self.enviar_logs("INFO", f'Cliente conectado usando IPv6 desde {res}')
+                            
+                            elif af == socket.AF_INET:
+                                print(f'\nCliente conectado usando IPv4 desde {res}')
+                                self.enviar_logs("INFO", f'Cliente conectado usando IPv4 desde {res}')
+                            
+                            self.puerto_cliente = res[1]
+
+                            mensaje_bienvenida = f"Cliente {self.puerto_cliente}"
+                            conn.sendall(mensaje_bienvenida.encode())
+
+                            client_handler = threading.Thread(target=self.manejar_cliente, args=(conn,), daemon=True)
+                            client_handler.start()
+
+                    except Exception as e:
+                        print(f"Error en el proceso hijo {os.getpid()} al manejar el socket: {e}")
+                        self.enviar_logs("ERROR", f"Error en el proceso hijo {os.getpid()} al manejar el socket: {e}")
                     
-                    if server_socket not in self.server_sockets:
-                        self.server_sockets.append(server_socket)
-                    else:
-                        pass
+                    finally:
+                        os._exit(0)  # Asegurarse de que el proceso hijo termine
 
-                    print(f"Servidor escuchando en {sockaddr[0]}:{self.port} ({address_type})")
-                    self.enviar_logs("INFO", f"Servidor iniciado en {sockaddr[0]}:{self.port} ({address_type})")
-
-                except Exception as e:
-                    print(f"Error al crear el socket para {sockaddr}: {e}")
-                    self.enviar_logs("ERROR", f"Error al crear el socket para {sockaddr}: {e}")
-
-            if not self.server_sockets:
+            if not addrinfos:
                 raise RuntimeError("No se pudieron crear sockets para ninguna de las direcciones")
-
-            while True:
-                try:
-                    readable, _, _ = select.select(self.server_sockets, [], []) # Selecciona los sockets que están listos para ser leídos.
-                    for s in readable:
-                        client_socket, client_address = s.accept()
-                        self.contador_clientes += 1
-                        print(f"\nCliente {self.contador_clientes} conectado desde {client_address}")
-                        self.enviar_logs("INFO", f"Cliente {self.contador_clientes} conectado desde {client_address}")
-
-                        mensaje_bienvenida = f"Cliente {self.contador_clientes}"
-                        client_socket.send(mensaje_bienvenida.encode())
-
-                        client_handler = threading.Thread(target=self.manejar_cliente, args=(client_socket,), daemon=True)
-                        client_handler.start()
-                except KeyboardInterrupt:
-                    self.enviar_logs("ERROR", "Servidor cerrado por interrupción de teclado")
-                    print("\nInterrupción por teclado detectada. Cerrando el servidor...")
-                    break
 
         except Exception as e:
             print(f"Error al iniciar el servidor: {e}")
             self.enviar_logs("ERROR", f"Error al iniciar el servidor: {e}")
+
+        # Bucle infinito para mantener el proceso principal activo
+        while True:
+            pass
 
     def manejar_cliente(self, client_socket):
         try:
             while True:
                 isbn = client_socket.recv(1024).decode()
                 if not isbn or isbn.lower() == 'q':
-                    print(f"\nCliente {self.contador_clientes} desconectado ")
-                    self.enviar_logs("INFO", f"Cliente {self.contador_clientes} desconectado")
+                    print(f"\nCliente {self.puerto_cliente} desconectado ")
+                    self.enviar_logs("INFO", f"Cliente {self.puerto_cliente} desconectado")
                     break
 
                 if len(isbn) == 13:
-                    print(f"\nCódigo ISBN recibido del cliente {self.contador_clientes}: {isbn}") 
-                    self.enviar_logs("INFO", f"ISBN recibido: {isbn} del cliente {self.contador_clientes}")
+                    print(f"\nCódigo ISBN recibido del cliente {self.puerto_cliente}: {isbn}") 
+                    self.enviar_logs("INFO", f"ISBN recibido: {isbn} del cliente {self.puerto_cliente}")
 
                     scraper = Scraping(isbn)
                     resultados = scraper.concurrent_scraping()
@@ -159,16 +163,16 @@ class Servidor:
                     if resultados:
                         respuesta = self.obtener_menor_precio(isbn, resultados)
                         client_socket.send(respuesta.encode())
-                        print(f"Respuesta enviada al cliente {self.contador_clientes}")
-                        self.enviar_logs("INFO", f"Respuesta enviada al cliente {self.contador_clientes}")
+                        print(f"Respuesta enviada al cliente {self.puerto_cliente}")
+                        self.enviar_logs("INFO", f"Respuesta enviada al cliente {self.puerto_cliente}")
                     else:
                         client_socket.send("Error al obtener precios".encode())
 
                 elif len(isbn) != 13:
                     client_socket.send("ISBN no válido".encode())
-                    self.enviar_logs("ERROR", f"ISBN no válido proporcionado por el cliente {self.contador_clientes}: {isbn}")
+                    self.enviar_logs("ERROR", f"ISBN no válido proporcionado por el cliente {self.puerto_cliente}: {isbn}")
                 else:
-                    self.enviar_logs("ERROR", f"ISBN no proporcionado por el cliente {self.contador_clientes}")
+                    self.enviar_logs("ERROR", f"ISBN no proporcionado por el cliente {self.puerto_cliente}")
         except Exception as e:
             print(f"Error al manejar cliente: {e}")
             self.enviar_logs("ERROR", f"Error al manejar cliente: {e}")
@@ -192,6 +196,7 @@ class Servidor:
             self.enviar_logs("ERROR", f"No se encuentra información del libro código ISBN13: {isbn}")
             return f"\nRespuesta del servidor:\nNo se encuentra información del libro código ISBN13 en ninguna de las librerías: {isbn}"
 
+
 def start_log_process(log_queue):
     logger = Logs()
     logger.iniciar_logs(log_queue)
@@ -199,13 +204,16 @@ def start_log_process(log_queue):
 if __name__ == "__main__":
     log_queue = Queue()
 
+    # Iniciar el proceso de logs como un proceso separado usando multiprocessing
     log_process = Process(target=start_log_process, args=(log_queue,))
     log_process.start()
-    print(f"\nPID del log_process: {log_process.pid}")
-    print("PID del servidor:", os.getpid()) 
+    print(f"PID del log_process: {log_process.pid}")
+    print("PID del PP:", os.getpid())
+
+
+    # Iniciar servidor
     server = Servidor(port=5555, log_queue=log_queue)
     server.start_server()
 
-    
-    log_queue.put(None)  
+    log_queue.put(None)  # Señal para terminar el proceso de logs
     log_process.join()
--- doc/descripcion.md
+++ doc/descripcion.md
@@ -4,3 +4,11 @@
 2. El servidor buscará el precio de ese libro en 3 librerías diferentes (Web Scraping), mostrando al cliente donde le conviene comprar el libro. 
 3. El servidor admite múltiples clientes de forma concurrente. (Cada cliente es un hilo separado)  
 4. El servidor almacena los logs en un archivo .log 
+
+
+# Listar puertos en escucha con netstat
+netstat -an | grep LISTEN
+
+# Probar ipv6 e ipv4 con Telnet
+telnet ::1  5555 
+telnet 127.0.0.1  5555
